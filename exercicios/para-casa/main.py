e74a97680431ba21a49b4c7f31b33201c607d38b
Daviny Letícia Vidal <daviny@vidal.dev.br>
2024-05-23 22:14:13 -0300

    add

--- exercicios/para-casa/main.py
+++ exercicios/para-casa/main.py
@@ -1,9 +1,27 @@
 import sqlite3
+import csv
 from banco.sql import execute_sql
+
+def import_csv(cursor, nome_tabela, nome_arquivo, commit):
+    
+    with open(nome_arquivo, newline='', encoding='utf-8') as csvFile:
+        leitor = csv.reader(csvFile)
+        next(leitor)
+        for linha in leitor:
+            cursor.execute(f"INSERT INTO {nome_tabela} (titulo,autor,ano,preco) VALUES (?,?,?,?)",(linha[0], linha[1], linha[2], linha[3]))
+        commit
 
 conn = sqlite3.Connection("livraria.db")
 cursor = conn.cursor()
 nome_tabela = "livros"
-dados_create = ["id INTEGER PRIMARY KEY", "nome TEXT", "idade INTEGER"]
+dados_create = ["id INTEGER PRIMARY KEY", "titulo TEXT", "autor Text", "ano integer", "preco real"]
+nome_arquivo = "livros.csv"
+
+
+
 
 execute_sql.create_sql(cursor, nome_tabela, dados_create)
+
+import_csv(cursor, nome_tabela, nome_arquivo, conn.commit)
+
+conn.close()
