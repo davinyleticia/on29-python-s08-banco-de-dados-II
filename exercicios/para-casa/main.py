58e78e5283fe1fd1472cc87e6b353ef53fef45a8
Daviny Letícia Vidal <daviny@vidal.dev.br>
2024-05-24 00:28:04 -0300

    add

--- exercicios/para-casa/main.py
+++ exercicios/para-casa/main.py
@@ -10,17 +10,18 @@ nome_tabela = "livros"
 dados_create = ["id INTEGER PRIMARY KEY", "titulo TEXT", "autor Text", "ano integer", "preco real"]
 nome_arquivo = "livros.csv"
 
-def import_csv(cursor, nome_tabela, nome_arquivo, commit):
-    
+def import_csv(cursor, nome_tabela, nome_arquivo):
     with open(nome_arquivo, newline='', encoding='utf-8') as csvFile:
         leitor = csv.reader(csvFile)
-        next(leitor)
+        next(leitor)  # Pular o cabeçalho
         for linha in leitor:
-            cursor.execute(f"INSERT INTO {nome_tabela} (titulo,autor,ano,preco) VALUES (?,?,?,?)",(linha[0], linha[1], linha[2], linha[3]))
-    commit
-
+            cursor.execute(f"INSERT INTO {nome_tabela} (titulo, autor, ano, preco) VALUES (?, ?, ?, ?)",
+                           (linha[0], linha[1], int(linha[2]), float(linha[3])))
+    conn.commit()
+    
 execute_sql.create_sql(cursor, nome_tabela, dados_create)
 
-import_csv(cursor, nome_tabela, nome_arquivo, conn.commit)
+import_csv(cursor, nome_tabela, nome_arquivo)
+
 
 conn.close()
